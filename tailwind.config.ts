38cc352cac11d3b35841afaaec919240a7838bcd
henzyd <henzydee@gmail.com>
2025-07-31 19:33:15 +0100

    updated tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,26 +3,6 @@ import type { Config } from "tailwindcss";
 export default {
   theme: {
     extend: {
-      fontFamily: {
-        ubuntu: ["Ubuntu", "sans-serif"],
-        playfair: ["Playfair Display", "serif"],
-      },
-      colors: {
-        primary: {
-          DEFAULT: "var(--primary)",
-          50: "var(--primary-50)",
-          100: "var(--primary-100)",
-          200: "var(--primary-200)",
-          300: "var(--primary-300)",
-          400: "var(--primary-400)",
-          500: "var(--primary-500)",
-          600: "var(--primary-600)",
-          700: "var(--primary-700)",
-          800: "var(--primary-800)",
-          900: "var(--primary-900)",
-          950: "var(--primary-950)",
-        },
-      },
       maxWidth: {
         App: "1300px",
       },
